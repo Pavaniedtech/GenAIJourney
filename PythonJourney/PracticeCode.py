2eb40328c07ae4b3fbb59d1cb3278ce31db15b2f
Pavaniedtech <your-email-linked-to-Pavaniedtech@domain.com>
2025-08-22 12:32:22 -0400

    This is my 2nd commit

---
+++ PythonJourney/PracticeCode.py
@@ -0,0 +1,13 @@
+print("I am practising my first python code")
+
+'''
+commenting in lines
+
+multiple line commenting
+'''
+#single line commenting
+
+# multipe line comments with # select command and forward slash
+# ands
+# sfdsf
+# asdasd
